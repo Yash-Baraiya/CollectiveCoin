6e59cf843e3c8cea69c05a9aed72f82686a3c16a
Yash_Baraiya <anilbaraiya1980@gmail.com>
2024-04-05 15:31:12 +0530

    implemented reset password functionality

--- Frontend/CollectiveCoin/src/app/resetpassword/resetpassword.component.ts
+++ Frontend/CollectiveCoin/src/app/resetpassword/resetpassword.component.ts
@@ -33,50 +33,50 @@ export class ResetpasswordComponent {
     });
   }
 
-  // resetpassword() {
-  //   if (this.passwordreset.valid) {
-  //     let bodyData = this.passwordreset.value;
-  //     let token = '';
-  //     this.activatedRoute.params.forEach((param) => {
-  //       token = param['token'];
-  //     });
+  resetpassword() {
+    if (this.passwordreset.valid) {
+      let bodyData = this.passwordreset.value;
+      let token = '';
+      this.activatedRoute.params.forEach((param) => {
+        token = param['token'];
+      });
 
-  //     this.http
-  //       .patch(
-  //         `http://localhost:8000/api/v1/CollectiveCoin/user/resetPassword/${token}`,
-  //         bodyData
-  //       )
-  //       .subscribe(
-  //         (resultData: any) => {
-  //           if (resultData) {
-  //             try {
-  //               alert('password reseted successfully');
-  //               this.router.navigate(['/login']);
-  //             } catch (error) {
-  //               console.log(error);
-  //               alert(resultData.error.message);
-  //             }
-  //           }
-  //         },
-  //         (error) => {
-  //           console.log(error);
-  //           if (error.error.message) {
-  //             console.log(error.error.message);
-  //             alert(error.error.message);
-  //             this.router.navigate(['/login']);
-  //           } else {
-  //             alert('An error occurred. Please try again later.');
-  //           }
-  //         }
-  //       );
-  //   } else {
-  //     alert('please fill the form correctly');
-  //   }
-  // }
+      this.http
+        .patch(
+          `http://localhost:8000/api/v1/CollectiveCoin/user/resetPassword/${token}`,
+          bodyData
+        )
+        .subscribe(
+          (resultData: any) => {
+            if (resultData) {
+              try {
+                alert('password reseted successfully');
+                this.router.navigate(['/login']);
+              } catch (error) {
+                console.log(error);
+                alert(resultData.error.message);
+              }
+            }
+          },
+          (error) => {
+            console.log(error);
+            if (error.error.message) {
+              console.log(error.error.message);
+              alert(error.error.message);
+              this.router.navigate(['/login']);
+            } else {
+              alert('An error occurred. Please try again later.');
+            }
+          }
+        );
+    } else {
+      alert('please fill the form correctly');
+    }
+  }
 
   save() {
     if (this.passwordreset.valid) {
-      //this.resetpassword();
+      this.resetpassword();
     } else {
       alert('please fill the form as directed');
     }
